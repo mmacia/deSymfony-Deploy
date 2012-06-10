7ba99ab793f7314b8bce42be00992d96390b5ca9
Moisés Maciá <moises.macia@ideup.com>
2012-06-11 00:13:34 +0200

    preparing controllers and routing

--- migrations-fixtures/src/deSymfony/DemoBundle/Controller/DefaultController.php
+++ migrations-fixtures/src/deSymfony/DemoBundle/Controller/DefaultController.php
@@ -6,14 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
+/**
+ * DefaultController
+ *
+ * @author Moisés Maciá <mmacia@gmail.com>
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/")
      * @Template()
+     *
+     * @return Response
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+        $em = $this->get('doctrine')->getEntityManager();
+        $paginator = $this->get('ideup.simple_paginator');
+
+        $repo = $em->getRepository('deSymfonyDemoBundle:Product');
+        $products = $repo->findAll();
+
+        $products = $paginator->paginate($products)->getResult();
+
+        return array(
+            'products'  => $products,
+            'paginator' => $paginator,
+        );
+    }
+
+    /**
+     * @param integer $productId
+     *
+     * @Route("/product/show/{id}")
+     * @Template()
+     *
+     * @return Response
+     */
+    public function productAction($productId)
+    {
+        $em = $this->get('doctrine')->getEntityManager();
+        $repo = $em->getRepository('deSymfonyDemoBundle:Product')->findOneById($productId);
+
+        return array('product' => $product);
     }
 }
